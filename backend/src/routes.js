76a3b1d182ff92b7b1f490cd1a8007290fa18f6a
LorenzoKersting <lorenzockersting@gmail.com>
2021-06-25 20:27:44 -0300

    backend | Create Spots filter by tech route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,9 +8,14 @@ const SpotController = require('./controllers/SpotController');
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
+//SESSION ROUTES
 routes.post('/sessions', SessionController.store); 
 
+//SPOTS ROUTES
+routes.get('/spots', SpotController.index); 
 routes.post('/spots', upload.single('thumbnail'),SpotController.store); 
+
+
 
 
 module.exports = routes;
