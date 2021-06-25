3385968a1b94e649992b909eade4c204497734cd
LorenzoKersting <lorenzockersting@gmail.com>
2021-06-25 19:18:23 -0300

    Create the backend folder - routes and session routes

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://lorenzo:lorenzo@cluster0.gslat.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+});
+
+
+app.use(express.json());
+app.use(routes);
+
+
+
+app.listen(3333);
