4a7fc0e04cf75b2b187fd21990ad157dc4c15f53
LorenzoKersting <lorenzockersting@gmail.com>
2021-06-28 12:42:19 -0300

    Backend| Changes on spots and routes

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 
 export default function Dashboard(){
+    const [spots, setSpots] = useState([]); 
+   
     useEffect(() => {
         async function loadSpots() {
             const user_id = localStorage.getItem('user');
@@ -9,13 +11,25 @@ export default function Dashboard(){
                 headers: { user_id }
             });
 
-            console.log(response.data);
+           setSpots(response.data);
         }
 
         loadSpots();
     }, []);
     
     
-    return <h1></h1>
+    return (
+        <>  
+        <ul className="spot-list">
+            {spots.map(spot => (
+                <li key={spot._id}> 
+                    <header />
+                    <strong>{spot.company}</strong>
+                    <span>{spot.price}</span>
+                </li>
+            ))}
+        </ul>
+        </>
+    )
     
 }
