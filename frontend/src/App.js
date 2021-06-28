d4196fb6553f79db2bb5e548094f7e0b2d8c4dea
LorenzoKersting <lorenzockersting@gmail.com>
2021-06-27 21:01:15 -0300

    Frontend| start first page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import React from 'react';
 import './App.css';
+
+
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-   <h1>Toma no cu</h1>
+    <div className="container">
+      <img src={logo}  alt="AirCnC" />
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+      </div>
+    </div>
   );
 }
 
